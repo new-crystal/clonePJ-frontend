88ae550812af714548f5405784522f47a3bd0ea0
Jisu Lee <jisu2281l@naver.com>
2022-08-19 18:54:32 +0900

    PR test

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,8 +5,8 @@ const Router = () => {
     return(
         <Routes>
             <Route path='/' element={<Main/>}/>
-            <Route path='/login' element={<Login/>}/>
             <Route path='/singup' element={<SignUp/>}/>
+            <Route path='/login' element={<Login/>}/>
             <Route path='/room/:roomId' element={<Room/>}/>
             <Route path='/*' element={<Navigate to="/"/>}/>
         </Routes>
