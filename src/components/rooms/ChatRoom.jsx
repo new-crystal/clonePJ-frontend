ca2d785651aa373030eb629b207dd63154a60746
crystal025 <oj3339@naver.com>
2022-08-25 10:07:12 +0900

    edit chat

--- src/components/rooms/ChatRoom.jsx
+++ src/components/rooms/ChatRoom.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import io from "socket.io-client";
+//import io from "socket.io-client";
 import { TextField } from "@material-ui/core";
 import styled from "styled-components";
 import MessageBox from "./MessageBox";
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom";
 import { serverUrl } from "../../redux/modules";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+//import jwt_decode from "jwt-decode";
 
 const ChatRoom = () => {
   const navigate = useNavigate();
@@ -18,22 +19,78 @@ const ChatRoom = () => {
   const [chats, setChats] = useState([]);
   const [room, setRoom] = useState(true);
   const token = localStorage.getItem("token");
+  //const payload = jwt_decode(token);
 
-  //socket 연결
-  const socket = io.connect("http://localhost:3000", {
-    path: "/socket.io",
-  });
+  // //socket 연결
+  // const socket = io.connect("http://localhost:3000", {
+  //   path: "/socket.io",
+  // });
+
+  // //socket연결
+  // const socket = io.connect("http://localhost:3000", {
+  //   cors: {
+  //     origin: "http://localhost:3000",
+  //     credentials: true,
+  //   },
+  //   transports: ["websocket"],
+  //   query: {
+  //     token,
+  //   },
+  // });
+
+  // useEffect(() => {
+  //   socket.emit(
+  //     "join",
+  //     { name: payload.nickname, room: roomData.roomName },
+  //     (error) => {
+  //       if (error) {
+  //         alert(error);
+  //       }
+  //     }
+  //   );
+  // }, []);
+
+  // useEffect(() => {
+  //   socket.on("message", (chatData) => {
+  //     setChats((chatData) => [...chatData, chatData]);
+  //   });
+  // }, []);
+
+  // //user가 채팅방입장시
+  // useEffect(() => {
+  //   socket.on("chatData", (data) => {
+  //     console.log(data);
+  //     setChats(chatData);
+  //     socket.emit("join-msg", `${socket["nickname"]}님께서 막 등장하셨습니다!`);
+  //   });
+  // }, []);
+
+  // //user 채팅방 입장시
+  // useEffect(() => {
+  //   socket.on("join-msg", (msg) => {
+  //     alert(msg);
+  //     setContent(msg);
+  //   });
+  // }, [socket]);
+  // //퇴장시
+  // useEffect(() => {
+  //   return () => {
+  //     if (socket) {
+  //       socket.disconnect();
+  //     }
+  //   };
+  // }, [socket]);
 
   //토큰 없을시 로그인으로
   useEffect(() => {
-    if (token === "") {
+    if (token === null) {
       return navigate("/login");
     }
   }, []);
 
   //socket에 방 전체 기존 메시지 수신
   const chatRoom = async () => {
-    socket.emit("newChat", roomId);
+    //socket.emit("newChat", roomId);
     const response = await axios.get(`${serverUrl}/chat/${roomId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -45,11 +102,7 @@ const ChatRoom = () => {
 
   useEffect(() => {
     chatRoom();
-  }, [room]);
-
-  useEffect(() => {
-    socket.on("newChat", chats);
-  }, []);
+  }, [room, chats]);
 
   //input 값 content에 넣어주고 chatData에 content 넣기
   const onTextChangeHandler = (e) => {
@@ -61,6 +114,7 @@ const ChatRoom = () => {
   const onMessageSubmit = async (e) => {
     e.preventDefault();
     if (content !== null) {
+      //socket.emit("sendMessage", chatData);
       await axios
         .post(`${serverUrl}/chat/${roomId}`, chatData, {
           headers: {
@@ -78,29 +132,12 @@ const ChatRoom = () => {
     setContent("");
   };
 
-  //user가 채팅방입장시
-  socket.on("join-room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket
-      .to(roomName)
-      .emit("join-msg", `${socket["nickname"]}님께서 막 등장하셨습니다!`);
-  });
-
-  //user 채팅방 입장시
-  useEffect(() => {
-    socket.on("join-msg", (msg) => {
-      alert(msg);
-      //setContent(msg);
-    });
-  }, [socket]);
-
   //채팅방 나갈시 확인
   const onClickHomeBtnHandler = () => {
     const result = window.confirm("채팅방을 나가시겠습니까?");
     if (result) {
       navigate("/");
-      socket.on("disconnect");
+      //socket.on("disconnect");
     }
   };
 
@@ -118,14 +155,6 @@ const ChatRoom = () => {
     }
   };
 
-  useEffect(() => {
-    return () => {
-      if (socket) {
-        socket.disconnect();
-      }
-    };
-  }, []);
-
   return (
     <Container onSubmit={(e) => onMessageSubmit(e)}>
       <div>
@@ -142,7 +171,7 @@ const ChatRoom = () => {
       <p>🟢online {chats.nickname}</p>
       <Messages>
         {chats?.map((chat) => {
-          return <MessageBox key={chat.chatId} chat={chat} socket={socket} />;
+          return <MessageBox key={chat.chatId} chat={chat} />;
         })}
       </Messages>
       <TextField
